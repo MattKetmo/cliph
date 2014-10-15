e293a21f8476ddc0de0c0931a5a9168cec6c1bab
Soufiane Ghzal <sghzal@gmail.com>
2014-10-15 14:27:50 +0200

    Add a set of verifactions to the bump script

--- bump-version.sh
+++ bump-version.sh
@@ -7,7 +7,37 @@ if [ $# -ne 1 ]; then
   exit 65
 fi
 
+
+# CHECK MASTER BRANCH
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$CURRENT_BRANCH" != "master" ]]; then
+  echo "You have to be on master branch currently on $CURRENT_BRANCH . Aborting"
+  exit 65
+fi
+
+# CHECK FORMAT OF THE TAG 
+php -r "if(preg_match('/^\d+\.\d+\.\d+(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?\$/',\$argv[1])) exit(0) ;else{ echo 'format of version tag is not invalid' . PHP_EOL ; exit(1);}" $1
+
+
+# CHECK jsawk COMMAND
+command -v jsawk >/dev/null 2>&1 || { echo "Error : Command jsawk is not installed on the system"; echo "See : https://github.com/micha/jsawk "; echo  "Exiting..." >&2; exit 65; }
+
+# CHECK js COMMAND
+command -v js >/dev/null 2>&1 || { echo "Error : Command js is not installed on the system"; echo "Should be fixed by installing spidermonkey "; echo  "Exiting..." >&2; exit 65; }
+
+# CHECK box COMMAND
+command -v box >/dev/null 2>&1 || { echo "Error : Command box is not installed on the system"; echo "See : https://github.com/box-project/box2 "; echo  "Exiting..." >&2; exit 65; }
+
+# CHECK python COMMAND
+command -v python >/dev/null 2>&1 || { echo "Error : Command python is not installed on the system"; echo  "Exiting..." >&2; exit 65; }
+
+# CHECK THAT WE CAN CHANGE BRANCH
+git checkout gh-pages
+git checkout --quiet master
+
+
 TAG=$1
+
 
 #
 # Tag & build master branch
